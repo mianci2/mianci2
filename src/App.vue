0a6aa06258cd1c998dc670fb0a42517deee02bdf
mianci2 <xingwangza@gmail.com>
2023-12-30 15:32:35 +0800

    创建虚拟数据表单 对表单内容进行查看、修改、删除

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,59 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import Edit from './components/Edit.vue'
+import axios from 'axios';
 
 // TODO: 列表渲染
+// 声明响应式list->调用接口获取数据->后端数据赋值给list->绑定到table组件
+const list = ref([])
+const getList = async () => {
+  // 接口调用
+  const res = await axios.get('/list')
+  //交给list
+  list.value = res.data
+}
 
+onMounted(() => getList())
 
 // TODO: 删除功能
+// 获取当前行的id->调用接口删除->重新获取列表
+const onDelete = async (id) => {
+  //console.log(id)
+  await axios.delete(`/del/${id}`)
+  // 重新获取列表
+  getList()
+}
+
 
 
 // TODO: 编辑功能
+// 打开弹窗-> 回填数据->更新数据
+const editRef = ref(null)
+const onEdit = (row) => {
+  // 打开弹窗
+  // 回填数据
+  editRef.value.open(row)
+
+
+}
 
 </script>
 
 <template>
   <div class="app">
-    <el-table :data="[{
-      id: 1,
-      name: 'jack',
-      place: 'none'
-    }]">
+    <el-table :data="list">
       <el-table-column label="ID" prop="id"></el-table-column>
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
-          <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+        <template #default="{ row }">
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="editRef" @on-update="getList" />
 </template>
 
 <style scoped>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,8 +1,46 @@
 <script setup>
 // TODO: 编辑
+import axios from 'axios'
 import { ref } from 'vue'
 // 弹框开关
 const dialogVisible = ref(false)
+
+//准备from
+const form = ref({
+  name: '',
+  place: '',
+  id: '',
+})
+const open = (row) => {
+  //console.log(row)
+  form.value.name = row.name
+  form.value.place = row.place
+  form.value.id = row.id
+  dialogVisible.value = true
+
+}
+//暴露open方法
+defineExpose({
+  open
+})
+
+const emit = defineEmits(['on-update'])
+const onUpdate = async () => {
+  //1.收集表单数据 调用接口
+  await axios.patch(`/edit/${form.value.id}`, {
+    name: form.value.name,
+    place: form.value.place
+  })
+  //2. 关闭弹窗
+  dialogVisible.value = false
+  //3. 通知父组件，列表刷新
+  emit('on-update')
+
+
+
+}
+
+
 
 
 </script>
@@ -11,16 +49,16 @@ const dialogVisible = ref(false)
   <el-dialog v-model="dialogVisible" title="编辑" width="400px">
     <el-form label-width="50px">
       <el-form-item label="姓名">
-        <el-input placeholder="请输入姓名" />
+        <el-input placeholder="请输入姓名" v-model="form.name" />
       </el-form-item>
       <el-form-item label="籍贯">
-        <el-input placeholder="请输入籍贯" />
+        <el-input placeholder="请输入籍贯" v-model="form.place" />
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+        <el-button type="primary" @click="onUpdate">确认</el-button>
       </span>
     </template>
   </el-dialog>
